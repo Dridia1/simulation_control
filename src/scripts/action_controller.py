c28c4730551c5620cd4cf6b1dbb9f005c91c620e
marcus <mgrimberg@live.se>
2018-12-14 12:03:10 +0100

    Improved State Machine + Code Cleanup

--- src/scripts/action_controller.py
+++ src/scripts/action_controller.py
@@ -5,34 +5,37 @@ import rospy
 import actionlib
 import mavros_state
 import time
-from simulation_control.msg import descend_on_objectAction, descend_on_objectGoal, detect_objectAction, detect_objectGoal, goto_positionAction, goto_positionGoal, short_grippersAction, short_grippersGoal, long_grippersAction, long_grippersGoal
+from simulation_control.msg import descend_on_objectAction, descend_on_objectGoal, detect_objectAction, \
+    detect_objectGoal, goto_positionAction, goto_positionGoal, short_grippersAction, short_grippersGoal, \
+    long_grippersAction, long_grippersGoal
 from std_msgs.msg import Float32
 from State import State
 from StateMachine import StateMachine
+
 # found below "import roslib"
 roslib.load_manifest('simulation_control')
+
 
 # Possible inputs: Object.Lost, Object.Found, Destination.Reached
 
 
 class TakeOff(State):
-
     takeoff = False
 
     def do_action(self):
-        print("TOstate: Start")
+        # print("TOstate: Start")
         goto_position_goal.destination.pose.position.z = 5
         goto_position_client.send_goal(goto_position_goal)
         # Will return True or False
         self.takeoff = goto_position_client.wait_for_result()
-        print("TOState succ: " + str(self.takeoff))
+        # print("TOState succ: " + str(self.takeoff))
         # Close long grippers to pickup position
         move_long_grippers(0.5000000000)
         print("TOState: moved long gripp. Done")
 
     def next_state(self):
         # if state_input == "":
-            # return "Fly To Search pos"
+        # return "Fly To Search pos"
         if self.takeoff:
             Drone.Flying.pos = [-105, 0, 3]
             return Drone.Flying
@@ -41,9 +44,8 @@ class TakeOff(State):
 
 
 class Flying(State):
-
     pos = [0, 0, 0]
-
+    CheckDroneVisible = False
     def do_action(self):
         print("Fstate: Flying to " + str(self.pos[0]) + ", " + str(self.pos[1]) + ", " + str(self.pos[2]))
         fly_to_pos(self.pos[0], self.pos[1], self.pos[2])
@@ -53,15 +55,97 @@ class Flying(State):
 
 
 class Searching(State):
+    found = False
+
     def do_action(self):
         print("Searching")
         detect_object_client.send_goal(detect_object_goal)
         detect_object_client.wait_for_result()
-        print(detect_object_client.get_result())
+        self.found = detect_object_client.get_result()
 
     def next_state(self):
-        #if state_input == "":
-            # return ""
+        if self.found:
+            Flying.pos = [detect_object_client.get_result().detected_position.pose.position.x,
+                          detect_object_client.get_result().detected_position.pose.position.y,
+                          detect_object_client.get_result().detected_position.pose.position.z]
+            return Drone.Flying
+        else:
+            return Searching
+
+
+class Centering(State):
+    def do_action(self):
+        print("Centering")
+
+    def next_state(self):
+        # if within x, return descend. If outside, return centering
+        return Drone.TakeOff
+
+
+# Regular Descend is fly_to_pos with lower z value.
+class Descend(State):
+
+    def do_action(self):
+        print("Descending")
+        goto_position_goal.destination.pose.position.z = 5
+        goto_position_client.send_goal(goto_position_goal)
+        # Will return True or False
+        self.ascended = goto_position_client.wait_for_result()
+
+    def next_state(self):
+        # if within x and below height y, return land. above height y descend. If outside, return centering
+        return Drone.TakeOff
+
+
+class Ascend(State):
+    def do_action(self):
+        print("Ascending")
+        goto_position_goal.destination.pose.position.z = 5
+        goto_position_client.send_goal(goto_position_goal)
+        # Will return True or False
+        self.ascended = goto_position_client.wait_for_result()
+
+    def next_state(self):
+        if self.ascended:
+            # If Camera sees drone Return Centering else return Ascend
+            return Drone.Ascend
+        else:
+            return Drone.Ascend
+
+
+class DescendOnDrone(State):
+    z = 0.5
+    succeeded = False
+
+    def do_action(self):
+        print("Descending")
+        descendOnObjectGoal = descend_on_objectGoal()
+        # descend_on_objectGoal.height = 2.0
+        descend_on_object_client.send_goal(descendOnObjectGoal)
+        succeeded = descend_on_object_client.wait_for_result()  # Returns if landing worked.
+
+    def next_state(self):
+        # if below height z, return Open grippers. above height y descend.
+        if self.succeeded:
+            return Drone.CloseGrippers()  # Landed on drone
+        return Drone.TakeOff
+
+
+class Land(State):
+    def do_action(self):
+        print("Land")
+
+    def next_state(self):
+        # Return Close grippers if drone is visible. If not return Takeoff?
+        return Drone.TakeOff
+
+
+class CloseGrippers(State):
+    def do_action(self):
+        print("CloseShortGrippers")
+
+    def next_state(self):
+        # Close grippers
         return Drone.TakeOff
 
 
@@ -71,10 +155,13 @@ class Drone(StateMachine):
         StateMachine.__init__(self, Drone.TakeOff)
 
 
-# Initialize the variables for the StateMachine
+# Initialize the states for the StateMachine
 Drone.TakeOff = TakeOff()
 Drone.Searching = Searching()
 Drone.Flying = Flying()
+Drone.Centering = Centering()
+Drone.CloseGrippers = CloseGrippers()
+Drone.Ascend = Ascend()
 drone_done = False
 
 
@@ -129,6 +216,10 @@ if __name__ == '__main__':
     detect_object_client.wait_for_server()
     detect_object_goal = detect_objectGoal()
 
+    # Init Descend on Object server
+    descend_on_object_client = actionlib.SimpleActionClient('descend_on_object', descend_on_objectAction)
+    descend_on_object_client.wait_for_server()
+
     # End of Initializing
 
     rospy.loginfo("Taking off")
@@ -139,8 +230,8 @@ if __name__ == '__main__':
     d = Drone()
     d.start()  # This will perform the TakeOff action.
     # while not drone_done:
-        # d.do_next_state()
-    
+    # d.do_next_state()
+
     rospy.loginfo("Takeoff succeeded: ")
 
     fly_to_pos(-105, 0, 3)
@@ -155,16 +246,17 @@ if __name__ == '__main__':
     fly_to_pos(detect_object_client.get_result().detected_position.pose.position.x,
                detect_object_client.get_result().detected_position.pose.position.y,
                detect_object_client.get_result().detected_position.pose.position.z)
-    rospy.loginfo("Is at position (x,y,z)=({}, {}, {})".format(detect_object_client.get_result().detected_position.pose.position.x,
-                                                               detect_object_client.get_result().detected_position.pose.position.y,
-                                                               detect_object_client.get_result().detected_position.pose.position.z))
+    rospy.loginfo("Is at position (x,y,z)=({}, {}, {})".format(
+        detect_object_client.get_result().detected_position.pose.position.x,
+        detect_object_client.get_result().detected_position.pose.position.y,
+        detect_object_client.get_result().detected_position.pose.position.z))
 
     rospy.loginfo("Descending on object")
     descend_on_object_client = actionlib.SimpleActionClient('descend_on_object', descend_on_objectAction)
     descend_on_object_client.wait_for_server()
     rospy.loginfo("Descending server started")
     descend_on_object_goal = descend_on_objectGoal()
-    descend_on_objectGoal = 2.0
+    # descend_on_objectGoal = 2.0
     descend_on_object_client.send_goal(descend_on_object_goal)
     descend_on_object_client.wait_for_result()
     if descend_on_object_client.get_result().position_reached.data:
@@ -200,7 +292,7 @@ if __name__ == '__main__':
     fly_to_pos(0, 105, 5)
     time.sleep(3)
 
-    rospy.loginfo("Deceding prior to drop off")
+    rospy.loginfo("Descending prior to drop off")
     fly_to_pos(0, 105, 0.5)
 
     # Open Long Grippers
